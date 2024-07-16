413632dda2858ed2eb560b57b4b34b349827de6c
joaohenriquefernandes <joaohenriquefernandes.dev@gmail.com>
2024-07-16 17:10:06 -0300

    feat: create transactions table components

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,12 +1,66 @@
 import { Header } from '../../components/Header';
 import { Summary } from '../../components/Summary';
 
+import {
+  PriceHighlight,
+  TransactionsContainer,
+  TransactionsTable,
+} from './styles';
+
 export function Transactions() {
   return (
     <>
       <Header />
       <Summary />
-      <h1>Main</h1>
+
+      <TransactionsContainer>
+        <TransactionsTable>
+          <tbody>
+            <tr>
+              <td width="40%">Desenvolvimento de site</td>
+              <td>
+                <PriceHighlight variant="income">R$ 15.000,00</PriceHighlight>
+              </td>
+              <td>Venda</td>
+              <td>12/04/2022</td>
+            </tr>
+            <tr>
+              <td width="40%">Desenvolvimento de site</td>
+              <td>
+                <PriceHighlight variant="income">R$ 15.000,00</PriceHighlight>
+              </td>
+              <td>Venda</td>
+              <td>12/04/2022</td>
+            </tr>
+            <tr>
+              <td width="40%">Hamburguer</td>
+              <td>
+                <PriceHighlight variant="outcome">
+                  - R$ 15.000,00
+                </PriceHighlight>
+              </td>
+              <td>Alimentação</td>
+              <td>10/04/2022</td>
+            </tr>
+            <tr>
+              <td width="40%">Desenvolvimento de site</td>
+              <td>
+                <PriceHighlight variant="income">R$ 15.000,00</PriceHighlight>
+              </td>
+              <td>Venda</td>
+              <td>12/04/2022</td>
+            </tr>
+            <tr>
+              <td width="40%">Desenvolvimento de site</td>
+              <td>
+                <PriceHighlight variant="income">R$ 15.000,00</PriceHighlight>
+              </td>
+              <td>Venda</td>
+              <td>12/04/2022</td>
+            </tr>
+          </tbody>
+        </TransactionsTable>
+      </TransactionsContainer>
     </>
   );
 }
